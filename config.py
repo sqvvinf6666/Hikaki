c6134a45c1774fcebb41037530efa2676a916e7a
sqvvinf6666 <bel1429@ukr.net>
2025-08-07 12:08:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "8369133318:AAFAJGQ2vGBW6KEWrnpH5DNovpsVhnxLKxU"
-ADMIN_IDS = [5005501869, 7999818491]  # Додано нового адміна
-SENDER_ID = 6831903905
+ADMIN_IDS = [7888789575, 7999818491]  # Додано нового адміна
+SENDER_ID = 7999818491
 GPT_PROMPT = "Ти бізнес-бот у телеграмі. Ти маєш відповідати, коли користувач не в мережі. Відповідай коротко і без форматування тексту.\nЗапит: "
-BOT_USERNAME = "SendCheekBot"
+BOT_USERNAME = "SeendChekBot"
